4ae8f096cc5a4f2f9cc4311571bb6a4d6f745fe5
Quentin Bossard <qbossard@tower-research.com>
2020-12-12 19:46:10 +0100

    refactor remote

--- src/remote2.cpp
+++ src/remote2.cpp
@@ -46,68 +46,6 @@ Adafruit_SSD1306 screen(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RST);
 #endif
 
 
-#include <Button2.h>
-
-#define BUTTON_1        17
-Button2 btn1(BUTTON_1);
-#define BUTTON_2        14
-Button2 btn2(BUTTON_2);
-#define BUTTON_3        12
-Button2 btn3(BUTTON_3);
-
-
-#include "WiFi.h"
-#include <esp_now.h>
-
-// uint8_t remoteAddress[] = {0x24, 0x6F, 0x28, 0x96, 0x4F, 0x54}; // TTGO T-Display
-// uint8_t trainAddress[] =  {0xCC, 0x50, 0xE3, 0xA8, 0x3D, 0x94}; // DOIT DEAD
-// uint8_t trainAddress[] =  {0xCC, 0x50, 0xE3, 0x95, 0xA3, 0xA0}; // DOIT DEAD
-// uint8_t trainAddress[] =  {0xCC, 0x50, 0xE3, 0xA1, 0x3F, 0x50}; // DOIT
-// future remote? 24:62:AB:DD:CD:10 LILYGO TS
-// uint8_t switchAddress[] =  {0x8C, 0xCE, 0x4E, 0xC7, 0xAF, 0xB0}; // 8C:CE:4E:C7:AF:B0 ESP8266 `wemos w/ battery
-
-#define TRAIN
-
-#if defined(TRAIN)
-uint8_t trainAddress[] =  {0xCC, 0x50, 0xE3, 0xA1, 0x3F, 0x50}; // DOIT
-// uint8_t trainAddress[] = {0x24, 0x6F, 0x28, 0x96, 0x4F, 0x54}; // TTGO T-Display
-#elif defined(SWITCH)
-// uint8_t trainAddress[] =  {0x8C, 0xCE, 0x4E, 0xC7, 0xAF, 0xB0}; // 8C:CE:4E:C7:AF:B0 ESP8266 `wemos w/ battery
-uint8_t trainAddress[] =  {0x24, 0x6F, 0x28, 0x96, 0x4F, 0x54}; // TTGO T-Display
-#endif
-
-typedef struct message {
-  char a[32];
-  int b;
-  float c;
-  String d;
-  bool e;
-} message;
-message mess;
-
-void send(int a_speed){
-    // Set values to send
-  strcpy(mess.a, "THIS IS A CHAR");
-  mess.b = a_speed;
-  mess.c = 1.2;
-  mess.d = "Hello";
-  mess.e = false;
-
-  // Send message via ESP-NOW
-  esp_err_t result = esp_now_send(trainAddress, (uint8_t *) &mess, sizeof(mess));
-   
-  if (result == ESP_OK) {
-    Serial.println("Sent with success");
-  }
-  else {
-    Serial.println("Error sending the data");
-  }
-}
-
-
-int last, value;
-
-
 void display_value(int value, int real_value) {
 #if defined(ST7735)
   screen.fillScreen(ST7735_BLACK);
@@ -144,28 +82,156 @@ void display_value(int value, int real_value) {
 
 }
 
+
+
+
+
+
+#include <Button2.h>
+
+#define BUTTON_1        17
+Button2 btn1(BUTTON_1);
+#define BUTTON_2        14
+Button2 btn2(BUTTON_2);
+#define BUTTON_3        12
+Button2 btn3(BUTTON_3);
+
+
+#include "WiFi.h"
+#include <esp_now.h>
+
+
+typedef struct message {
+  char a[32];
+  int b;
+  float c;
+  String d;
+  bool e;
+} message;
+
+template<class T> inline Print &operator <<(Print &obj, T arg) { obj.print(arg); return obj; }
+
+
+class ControlledDevice {
+  private :
+    message mess;
+    int value_cap;
+    int value_multiplier;
+    int value_offset;
+
+  public:
+    uint8_t* address;
+    int value;
+    int last_value;
+
+    ControlledDevice(uint8_t* an_address, int a_cap, int a_multiplier, int an_offset) {
+      this->last_value = this->value = 0;
+      this->address = an_address;
+      this->value_cap = a_cap;
+      this->value_multiplier = a_multiplier;
+      this->value_offset = an_offset;
+
+      strcpy(this->mess.a, "THIS IS A CHAR");
+      this->mess.b = this->value;
+      this->mess.c = 1.2;
+      this->mess.d = "Hello";
+      this->mess.e = false;
+    }
+
+    void inc_value(){
+      this->value = max(min(this->value+1, this->value_cap), -this->value_cap);
+      if (this->value != this->last_value) {
+        // trigger change
+        this->send_value();
+        display_value(this->value, this->last_value);
+        this->last_value = this->value;
+      }
+    }
+    void dec_value(){
+      this->value = max(min(this->value-1, this->value_cap), -this->value_cap);
+      if (this->value != this->last_value) {
+        // trigger change
+        this->send_value();
+        display_value(this->value, this->last_value);
+        this->last_value = this->value;
+      }
+    }
+    void neutral(){
+      this->value = max(min(0, this->value_cap), -this->value_cap);
+      if (this->value != this->last_value) {
+        // trigger change
+        this->send_value();
+        display_value(this->value, this->last_value);
+        this->last_value = this->value;
+      }
+    }
+    void send_value(){
+
+      int value_to_send = this->value * this->value_multiplier + (this->value>0?1:(this->value<0?-1:0)) * this->value_offset;
+      this->mess.b = value_to_send;
+
+      Serial << "sending " << value_to_send << " (value " << this->value << ") "; 
+
+      // Send message via ESP-NOW
+      esp_err_t result = esp_now_send(this->address, (uint8_t *) &this->mess, sizeof(this->mess));
+      
+      Serial << ( (result == ESP_OK)? " OK\n": " ERR\n");
+    }
+};
+
+
+
+
+// uint8_t ad[] =  {0xCC, 0x50, 0xE3, 0xA1, 0x3F, 0x50}; // DOIT
+// ControlledDevice cur_device(ad, 9, 5, 155);
+
+uint8_t ad[] =  {0x24, 0x6F, 0x28, 0x96, 0x4F, 0x54}; // TTGO T-Display
+ControlledDevice cur_device(ad, 9, 5, 155);
+
+// 24:A1:60:2E:07:89  WEMOS D1 Mini (1)
+// ControlledDevice cur_device(ad, 1, 255, 1);
+
+
+void stop_handler(Button2& btn) {
+    switch (btn.getClickType()) {
+        case SINGLE_CLICK:
+            cur_device.neutral();
+            break;
+        // case DOUBLE_CLICK:
+        //     Serial.print("double ");
+        //     break;
+        case TRIPLE_CLICK:
+            Serial.println("stopping all devices");
+            break;
+        case LONG_CLICK:
+            cur_device.neutral();
+            break;
+    }
+}
+
+
 void setup(void) {
 
   Serial.begin(115200);
 
 
   btn1.setPressedHandler([](Button2 & b) {
-    Serial.println("button 1 --> --");
-    value--;
+    cur_device.dec_value();
   });
   btn3.setPressedHandler([](Button2 & b) {
-    value++;
-    Serial.println("button 3 -> ++");
+    cur_device.inc_value();
   });
 
-  btn2.setPressedHandler([](Button2 & b) {
-    value = 0;
-    Serial.println("button 2 -> 0");
-  });
+
+  btn2.setClickHandler(stop_handler);
+  btn2.setLongClickHandler(stop_handler);
+  btn2.setDoubleClickHandler(stop_handler);
+  btn2.setTripleClickHandler(stop_handler);
+
 
   WiFi.mode(WIFI_MODE_STA);
   if (esp_now_init() != ESP_OK) {
-    Serial.println("Error initializing ESP-NOW");
+    Serial << "Error initializing ESP-NOW\n";
     return;
   }
   Serial.print("MAC "); Serial.println(WiFi.macAddress());
@@ -173,7 +239,7 @@ void setup(void) {
 
   // Register peer
   esp_now_peer_info_t peerInfo;
-  memcpy(peerInfo.peer_addr, trainAddress, 6);
+  memcpy(peerInfo.peer_addr, cur_device.address, 6);
   peerInfo.channel = 0;  
   peerInfo.encrypt = false;
   
@@ -183,7 +249,7 @@ void setup(void) {
     return;
   }
 
-  send(0);
+  cur_device.send_value();
 
 
 
@@ -234,39 +300,14 @@ void setup(void) {
   screen.print("MAC "); screen.println(WiFi.macAddress());
   screen.display();
 
-  // sleep(2000);
-
-
-
-
-
 }
   
 
 void loop() {
 
-  
   btn1.loop();
   btn2.loop();
   btn3.loop();
-
-  if (value != last) {
-    Serial.println(value);
-    last = value;
-
-#if defined(TRAIN)
-    // value -20 --> 20
-    value =  min(9,max(-9, value));
-    int speed = 5 * value + (value>0?1:(value<0?-1:0)) * 155;
-#elif defined(SWITCH)
-    value =  min(1,max(0, value));
-    int speed = 65*value+1;
-#endif
-
-    send(speed);
-    display_value(value, speed);
-
-  }
 
 }
 
